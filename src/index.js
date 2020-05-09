f3ecbe603d9c1fe9cbba696212ac87c2e5331d00
Diwakar Sharma <diwakarsharma95@gmail.com>
2020-05-10 08:15:23 +1000

    Function Components and Class Components

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
+import React from 'react'
+import ReactDOM from 'react-dom'
+import Student from './Student'
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(<Student name='Dave' />, document.getElementById('root'))
